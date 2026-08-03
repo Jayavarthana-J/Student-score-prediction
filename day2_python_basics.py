12d87253c5f9d0f792785e1e05828b08ef60c73c
Jayavarthana-J <buellajay715@gmail.com>
2026-08-03 13:43:43 +0530

    Fix grade thresholds for letter grades

--- day2_python_basics.py
+++ day2_python_basics.py
@@ -43,9 +43,9 @@ print(predict_score_naive(6))
 def grade(score):
     if score >= 90:
         return "A"
-    elif score >= 75:
+    elif score >= 80:
         return "B"
-    elif score >= 60:
+    elif score >= 70:
         return "C"
     else:
         return "D"
